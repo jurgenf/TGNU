e9b429be905d493eaad60f099f46a918ee08ce46
jurgenf <info@jurgenfranse.nl>
2009-12-16 21:26:41 +0100

    The terminal class is ready to use and I have written a simple start cinematic to start te game.

--- GameController.java
+++ GameController.java
@@ -8,6 +8,8 @@
 public class GameController
 {
     private Terminal terminal;
+    private String username;
+    
     //private Shell shell;
 
     /**
@@ -16,8 +18,8 @@ public class GameController
     public GameController()
     {
         //initialize the instance variables
-        terminal = new Terminal();
-       // shell = new Shell();
+        terminal = new Terminal(this);
+        // shell = new Shell();
         
         //output the start screen
         terminal.print("******************************************************");
@@ -26,19 +28,92 @@ public class GameController
         terminal.print("*     | | | |  __|  \\| | |  | |                      *");
         terminal.print("*     | | | | |_ | . ` | |  | |                      *");
         terminal.print("*     | | | |__| | |\\  | |__| |                      *");
-        terminal.print("*     |_|  \\_____|_| \\_|\\____/                      *");
+        terminal.print("*     |_|  \\_____|_| \\_|\\____/                       *");
         terminal.print("*        This Game is Not Unix version 0.1           *");
         terminal.print("*****************************************************");
         terminal.print("");
-        terminal.print("");
         terminal.print("Please enter your username:");
+        username = terminal.getUsername();
+        this.initTheGame();
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * Initialize the game!
+     *
      */
-
+    public void initTheGame()
+    {
+        terminal.setPrompt(username+"@h4ckst4ti0n> ");
+        
+        this.hackCinematic();
+        terminal.print("Welcome to TGNU, the game now starts!");
+        terminal.print("You have broken into the fileserver of Area51, the files are mounted in the folder 'mainframe'");
+        terminal.print("Your task is to seek out and copy classified data about aliens to your local harddrive");
+        terminal.print("Put the collected data in your home folder (/home/"+username+"/)");
+        terminal.print("Type 'help' for more info about the commands you can use.");
+        terminal.getInput();
+    }
+    
+    /**
+     * Play a cinematic where the user sees itself hacking the Area51 mainframe
+     *
+     */
+    public void hackCinematic()
+    {
+        String prompt = terminal.getPrompt();
+        terminal.printInline(prompt);
+        terminal.printAsTyped("ssh serv1425.area51.gov");
+        terminal.print("Welcome to the Area51 fileserver");
+        this.sleep(1000);
+        terminal.print("Please enter username and password");
+        terminal.printInline("username: ");
+        this.sleep(1000);
+        terminal.printAsTyped("root");
+        this.sleep(1000);
+        terminal.printInline("password: ");
+        this.sleep(4000);
+        terminal.printAsTyped("Alien1");
+        this.sleep(3000);
+        terminal.print("Welcome root you now have full system access.");
+        this.sleep(2000);
+        terminal.printInline("root@serv1425# ");
+        this.sleep(3000);
+        terminal.printAsTyped("exit");
+        this.sleep(1000);
+        terminal.printInline(prompt);
+        this.sleep(1000);
+        terminal.printAsTyped("sshfs root@serv1425.area51.gov:/ /mainframe");
+        terminal.printInline("password: ");
+        this.sleep(800);
+        terminal.printAsTyped("Alien1");
+        this.sleep(500);
+    } 
+    
+    
+    /**
+     * Return the players username
+     *
+     * @return  the players username 
+     */
+    public String getUsername()
+    {
+        return username;
+    }
+    
+    /**
+     * sleep the program
+     * used to simulate the feeling of the mainframe/computer processing the input
+     * 
+     * @param   int the time to sleep in miliseconds
+     */    
+    public void sleep(int sleepTime) 
+    {
+        try {
+            Thread.sleep(sleepTime);
+        }
+        catch(Exception e) {
+             System.out.println("Fatal error in the game: "+e);
+        }
+    }    
+               
 }
--- Terminal.java
+++ Terminal.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 /**
  * This class implements a terminal from wich the program talks to the terminal.
@@ -9,17 +10,21 @@ public class Terminal
 {
 
     private Scanner terminalReader;
+    private String prompt;
+    private GameController theGame;
     
     /**
      * Constructor for objects of class Terminal
      */
-    public Terminal()
+    public Terminal(GameController theGame)
     {
         terminalReader = new Scanner(System.in);
+        prompt = ">";
+        this.theGame = theGame;
     }
 
     /**
-     * Print a line of data to the terminal
+     * Print a line of text to the terminal
      * 
      * @param  text   the text to print to the terminal
      */
@@ -29,22 +34,83 @@ public class Terminal
     }
     
     /**
+     * Print a line of tekst without printing a newline.
+     * 
+     * @param   text   the text to print to the terminal
+     */    
+    public void printInline(String text)
+    {
+        System.out.print(text);
+    }
+    
+    /**
+     * Print a line of text to the terminal as it was typed (typewriter effect)
+     * 
+     * @param  text   the text to print to the terminal
+     */
+    public void printAsTyped(String text)
+    {
+        char[] textArray = text.toCharArray();
+        for(char character : textArray)
+        {
+            System.out.print(character);
+            theGame.sleep(100);
+        }
+        System.out.print("\n");
+    }
+
+    /**
+     * Return the username the user logs in with and return it
+     * 
+     * @return  the username the user choosed
+     */    
+    public String getUsername() 
+    {
+        System.out.print(prompt);
+        String username = terminalReader.nextLine().trim().toLowerCase();
+        return username;
+    }  
+    
+    /**
+     * Set the prompt (the tekst before the cursor where you are suppose to type) to the given tekst
+     * 
+     * @param  prompt the new prompt tekst to use
+     */    
+    public void setPrompt(String prompt) 
+    {
+        this.prompt = prompt;
+    }
+    
+    /**
+     * Get the prompt (the tekst before the cursor where you are suppose to type)
+     * This can be used in cinematics to fake a prompt
+     * 
+     * @return  the currently used prompt tekst
+     */    
+    public String getPrompt() 
+    {
+        return prompt;
+    }
+    
+    
+    /**
      * Return the user input from the terminal as a ArrayList containing all words that where typed in the console.
      * 
      * @return  an ArrayList containing strings that where typed into the terminal
      */    
     public ArrayList<String> getInput() 
     {
-        System.out.print("> ");                // print prompt
-        String inputLine = reader.nextLine().trim().toLowerCase();
+        System.out.print(prompt);
+        String inputLine = terminalReader.nextLine().trim().toLowerCase();
 
-        String[] wordArray = inputLine.split(" ");  // split at spaces
+        String[] wordArray = inputLine.split(" ");
 
-        // add words from array into hashset 
-        HashSet<String> words = new HashSet<String>();
+        ArrayList<String> words = new ArrayList<String>();
         for(String word : wordArray) {
             words.add(word);
         }
         return words;
     }
 }
+    
+   
