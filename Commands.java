779f61e9a9a2c5f0865343168f21a4eabc7d405d
Hotlander <hotlander@gmail.com>
2010-01-11 10:48:20 +0100

    Update Command class, Commands class and Shell class. To fix empty classes
    Signed-off-by: Hotlander <hotlander@gmail.com>

---
+++ Commands.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Commands here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public enum Commands
+{
+    // A value for each command word, plus one for unrecognised
+    // commands.
+    HELP, COPY, QUIT, CD, CAT, UPTIME, LIST, UNKNOWN;
+}
